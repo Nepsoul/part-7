689c099f8a06a5447405d5eb13cf21fd3bc1f0a2
nepsoul <nirumagar901@gmail.com>
2023-02-09 18:24:41 +0545

    chore: set up of ultimate-hooks

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -1,5 +1,72 @@
-function App() {
-  return <div>new project ultimate-hook</div>;
-}
+import { useState, useEffect } from "react";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return { type, value, onChange };
+};
+
+const useResource = (baseUrl) => {
+  const [resource, setResource] = useState([]);
+
+  const create = (resource) => {};
+
+  const service = {
+    create,
+  };
+
+  return [resource, service];
+};
+
+const App = () => {
+  const content = useField("text");
+  const name = useField("text");
+  const number = useField("text");
+
+  const [notes, noteService] = useResource("http//localhost/:3005/notes");
+  const [persons, personService] = useResource("http//localhost/:3005/persons");
+  const handleNoteSubmit = (event) => {
+    event.prevent.default();
+    noteService.create({ content: content.value });
+  };
+
+  const handlePersonSubmit = (event) => {
+    event.prevent.default();
+    personService.create({ name: name.value, number: number.value });
+  };
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name
+        <input {...name} />
+        <br />
+        number
+        <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name}
+          {n.number}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
